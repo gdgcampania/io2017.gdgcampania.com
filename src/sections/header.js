46429019b1cfc8e5d54058da8d1ca2dc42066e95
Patrick Arminio <patrick.arminio@gmail.com>
2017-05-03 22:51:29 +0100

    Split into sections

---
+++ src/sections/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Section from "../components/section";
+import Wrapper from "../components/wrapper";
+
+export default () => (
+  <Section centered variant="header">
+    <Wrapper>
+      <h1>Google IO extended Campania</h1>
+      <h2>17 Maggio 2017</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit
+        Cumque quos ratione optio blanditis deserunt saepe iure
+        unt odit atque eius voluptatum labore ex
+      </p>
+
+      <button>Registrati</button>
+    </Wrapper>
+  </Section>
+);
