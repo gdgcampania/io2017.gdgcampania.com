52b4033d51b681faf18f858a97096e3698cf5113
Patrick Arminio <patrick.arminio@gmail.com>
2017-05-07 15:47:01 +0100

    Add header animation

--- src/components/title.js
+++ src/components/title.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 export default styled.h1`
-    font-size: 30px;
-    margin-bottom: 0.5em;
-    text-transform: uppercase;
+  font-family: Ubuntu;
+  font-size: ${props => (typeof props.size === "undefined" ? "30px" : props.size)};
+  font-weight: 500;
+  margin-bottom: ${props => (typeof props.margin === "undefined" ? "0.5em" : props.margin)};
+
+  opacity: ${props => (props.transparent ? 0 : 1)};
+
+  ${props => props.color && `color: ${props.color}`}
 `;
